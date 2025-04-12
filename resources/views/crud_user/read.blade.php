b4e2ae4c4c1b3e1a68f858ee3c3757b372034c15
hovanhieu187 <hovanhieu.tdc2223@gmail.com>
2025-04-12 08:04:35 +0700

    add source laravel

---
+++ resources/views/crud_user/read.blade.php
@@ -0,0 +1,30 @@
+@extends('dashboard')
+
+@section('content')
+    <main class="login-form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Address</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{$messi->id}}</td>
+                            <td>{{$messi->name}}</td>
+                            <td>{{$messi->phone}}</td>
+                            <td>{{$messi->address}}</td>
+                            <td>{{$messi->email}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+@endsection
